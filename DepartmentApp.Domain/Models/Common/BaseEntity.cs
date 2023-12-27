63073c8d196756dd4423b487751a68782dddc7c9
code810 <nadirssh@code.edu.az>
2023-12-27 23:13:11 +0400

    Start task

---
+++ DepartmentApp.Domain/Models/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace DepartmentApp.Domain.Models.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public DateTime DeletedDate { get; set; }
+    }
+}
